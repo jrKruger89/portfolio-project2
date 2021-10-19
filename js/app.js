421c7a4df9cf69dd080589af66e2f4ec85089039
jrKruger89 <35734267+jrKruger89@users.noreply.github.com>
2021-10-19 12:58:56 +0200

    Added some text

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 import "./router.js";
-console.log("router is running...");
+console.log("Router is running...");
 /**
  * Toggle mewnu for mobile devices
  */
@@ -10,27 +10,46 @@ const main = document.querySelector("main");
 const footer = document.querySelector("footer");
 
 let toggleMenu = () => {
-    burger.classList.toggle("open");
+  burger.classList.toggle("open");
 
-    if (burger.classList.contains("open")) {
-        navlist.style.left = "0px";
-        main.classList.add("blur");
-        footer.classList.add("blur");
-    } else {
-        navlist.style.left = "-220px";
-        main.classList.remove("blur");
-        footer.classList.remove("blur");
-    }
+  if (burger.classList.contains("open")) {
+    navlist.style.left = "0px";
+    main.classList.add("blur");
+    footer.classList.add("blur");
+  } else {
+    navlist.style.left = "-220px";
+    main.classList.remove("blur");
+    footer.classList.remove("blur");
+  }
 };
 
 window.toggleMenu = () => toggleMenu();
 
 let displayAge = () => {
-    let age_tag = document.querySelector(".age");
-    const myBirthDate = new Date(1989, 7, 15);
-    let diff = Date.now() - myBirthDate.getTime();
-    let age = new Date(diff);
-    age_tag.innerHTML = Math.abs(age.getUTCFullYear() - 1970);
+  const myBirthDate = new Date(1989, 7, 15);
+  let diff = Date.now() - myBirthDate.getTime();
+  let age = new Date(diff);
+  return Math.abs(age.getUTCFullYear() - 1970);
 };
+
+let display_about_text =
+  /*html*/
+  `Mit navn er <b>Jesper Ring Krüger</b>,
+    jeg er ${displayAge()} år.
+    Til daglig er jeg multimediedesign
+    studerende og er i gang med mit 3. semester
+    på Erhvervsakademi Århus.<br/><br/>
+    Når jeg ikke sidder ved computeren
+    bruger jeg en stor portion af min
+    tid på sport. Jeg er en del af det danske
+    landshold i kørestolsrugby og deltog,
+    i sommers, i de Paralympiske Lege i Tokyo.<br/><br/>
+    Jeg har en humoristisk tilgang til livet.
+    Dette betyder bestemt ikke at jeg ikke er seriøs
+    omkring mit arbejde, studieliv såvel som sportsliv.
+    Jeg er dedikeret til den opgave som er stillet foran mig.`;
+
+let age_tag = document.querySelector(".about-text");
+age_tag.innerHTML = display_about_text;
 
 window.displayAge = () => displayAge();
